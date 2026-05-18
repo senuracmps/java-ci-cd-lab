2cef74311e68d0e632bbc6289e83b7276f4775e9
senuracmps@gmail.com <senuracmps@gmail.com>
2026-05-18 14:56:52 +0530

    Updated Calculator

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,7 +1,28 @@
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator cal=new Calculator();
-        if(cal.add(2,3)==5){
+        Calculator cal1=new Calculator();
+        if(cal1.add(2,3)==5){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        Calculator cal2=new Calculator();
+        if(cal2.sub(3,2)==1){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        Calculator cal3=new Calculator();
+        if(cal3.mul(2,3)==6){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+
+        Calculator cal4=new Calculator();
+        if(cal4.div(6,3)==2){
             System.out.println("Test Passed");
         }else{
             System.out.println("Test Failed");
